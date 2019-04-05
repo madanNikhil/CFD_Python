9ed55af2d0c60b0e68df50ea44a5a3ca6a056d69
Nikhil Madan <mr.nikhil.madan@gmail.com>
2019-04-05 10:11:16 +0200

    Commented this file in details

--- TrappedCavityFlow.py
+++ TrappedCavityFlow.py
@@ -3,24 +3,37 @@
 Created on Thu Jan 08 14:45:38 2015
 
 @author: nikhil madan
+
+# CFD Tool to analyze Trapped Vortex Combution chamber design - Master thesis at
+
+Warsaw University of Technology, Poland, March 2015.
+
+Discretization of Navier Stokes Eqn. and solving it numerically on the defined mesh.
 """
+# import things
 
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 
-nx = 41
-ny = 41
-nt = 100
-nit=50
-c = 1
-dx = 8.0/(nx-1)
-dy = 8.0/(ny-1)
+# define parameters
+
+nx = 41			# no. of nodes in x-direction
+ny = 41			# no. of nodes in y-direction
+nt = 100		# no. of time steps
+nit=50			# no. of iterations
+c = 1			# normalized input velocity
+dx = 8.0/(nx-1) # unit x-distance
+dy = 8.0/(ny-1)	# unit y-distance
+
+# create mesh
+
 x = np.linspace(0,8,nx)
 y = np.linspace(0,12,ny)
 Y,X = np.meshgrid(y,x)
 
+# fluid properties -- air in this case
 rho = 1
 nu = .1
 dt = .001
@@ -31,14 +44,16 @@ c = 0.71
 h  = 280.3
 gamma = 1.4
 
+#init velocities, pressures, tempratures
+
 u = np.zeros((ny, nx))
 v = np.zeros((ny, nx))
 p = np.zeros((ny, nx)) 
-b = np.zeros((ny, nx))
+b = np.zeros((ny, nx)) # intermediatory variable for the ease of splitting a longer equation
 T = np.zeros((ny, nx))
 
 
-
+# function to evaluate intermediatory variable
 
 def buildUpB(b, rho, dt, u, v, dx, dy):
     
@@ -48,6 +63,8 @@ def buildUpB(b, rho, dt, u, v, dx, dy):
 		((v[1:-1,2:]-v[1:-1,0:-2])/(2*dy))**2)
 	
     return b
+
+# function to evaluate pressure variable
 
 def presPoisson(p, dx, dy, b):
     pn = np.empty_like(p)
@@ -67,6 +84,8 @@ def presPoisson(p, dx, dy, b):
         
     return p
     
+# function to evaluate output parameters
+
 def trappedCavityFlow(nt, u, v, dt, dx, dy, p, rho, nu, T):
     un = np.empty_like(u)
     vn = np.empty_like(v)
@@ -83,12 +102,16 @@ def trappedCavityFlow(nt, u, v, dt, dx, dy, p, rho, nu, T):
         b = buildUpB(b, rho, dt, u, v, dx, dy)
         p = presPoisson(p, dx, dy, b)
         
+        # x-velocity component
+
         u[1:-1,1:-1] = un[1:-1,1:-1]-\
             un[1:-1,1:-1]*dt/dx*(un[1:-1,1:-1]-un[0:-2,1:-1])-\
             vn[1:-1,1:-1]*dt/dy*(un[1:-1,1:-1]-un[1:-1,0:-2])-\
             dt/(2*rho*dx)*(p[2:,1:-1]-p[0:-2,1:-1])+\
             nu*(dt/dx**2*(un[2:,1:-1]-2*un[1:-1,1:-1]+un[0:-2,1:-1])+\
             dt/dy**2*(un[1:-1,2:]-2*un[1:-1,1:-1]+un[1:-1,0:-2]))
+
+         # y-velocity component   
 	
         v[1:-1,1:-1] = vn[1:-1,1:-1]-\
             un[1:-1,1:-1]*dt/dx*(vn[1:-1,1:-1]-vn[0:-2,1:-1])-\
@@ -96,13 +119,15 @@ def trappedCavityFlow(nt, u, v, dt, dx, dy, p, rho, nu, T):
             dt/(2*rho*dy)*(p[1:-1,2:]-p[1:-1,0:-2])+\
             nu*(dt/dx**2*(vn[2:,1:-1]-2*vn[1:-1,1:-1]+vn[0:-2,1:-1])+\
             (dt/dy**2*(vn[1:-1,2:]-2*vn[1:-1,1:-1]+vn[1:-1,0:-2])))
-            
+         
+         # thermal component   
+
         T[1:-1,1:-1]=Tn[1:-1,1:-1]-\
             (2.0/(c*(1-2*gamma)))*un[1:-1,1:-1]*(u[1:-1,1:-1]-un[1:-1,1:-1])-(2.0/(c*(1-2*gamma)))*vn[1:-1,1:-1]*(v[1:-1,1:-1]-vn[1:-1,1:-1])+\
             (2*k*dt/(c*(1-2*gamma)*rho*dx**2))*(Tn[2:,1:-1]-2*Tn[1:-1,1:-1]+Tn[0:-2,1:-1])+\
             (2*k*dt/(c*(1-2*gamma)*rho*dy**2))*(Tn[1:-1,2:]-2*Tn[1:-1,1:-1]+Tn[1:-1,0:-2])
             
-        
+        # Defined boundry conditions
         
         u[0,:((ny-1)/3)] = 0
         u[0,((ny-1)/3):(2*(ny-1)/3)]= 3
@@ -125,6 +150,8 @@ def trappedCavityFlow(nt, u, v, dt, dx, dy, p, rho, nu, T):
         T[0,(5*(ny-1)/6):(ny)]= 1000
         
     return u, v, p, T
+
+# Output of results
 
 u = np.zeros((ny, nx))
 v = np.zeros((ny, nx))
